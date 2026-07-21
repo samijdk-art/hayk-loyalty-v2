7f820d3735c7cf244db630f1b14746408d061d8e
samijdk-art <Sami.jdk@gmail.com>
2026-07-21 18:03:40 +0330

    Fix window is not defined

--- app/admin/CustomerQR.tsx
+++ app/admin/CustomerQR.tsx
@@ -1,42 +1,31 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
-
 
 export default function CustomerQR({
   id,
 }: {
   id: string;
 }) {
+  const [url, setUrl] = useState("");
 
+  useEffect(() => {
+    setUrl(`${window.location.origin}/customer/${id}`);
+  }, [id]);
 
-  const url = `${window.location.origin}/customer/${id}`;
-
-
+  if (!url) return null;
 
   return (
-
     <div className="mt-4">
-
-
       <QRCodeCanvas
-
         value={url}
-
         size={150}
-
       />
 
-
       <p className="text-xs mt-2 text-gray-500">
-
         Scan Customer QR
-
       </p>
-
-
     </div>
-
   );
-
 }
